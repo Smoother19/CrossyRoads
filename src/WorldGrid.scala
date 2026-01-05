da4c48f8829236f1c5fc47c73485371a442f1259
noah <noahabraao.pro@gmail.com>
2026-01-05 16:52:31 +0100

    Logs movemment + colors

--- src/WorldGrid.scala
+++ src/WorldGrid.scala
@@ -4,6 +4,9 @@ class WorldGrid(length: Int, width: Int) {
 
   var grid: Array[Array[Int]] = Array.ofDim(length, width)
 
+  // 1 = Vers la Droite, -1 = Vers la Gauche
+  var rowDirections: Array[Int] = Array.fill(length)(1)
+
   def fillEmptyGrid(emptyGrid: Array[Array[Int]]): Array[Array[Int]] = {
     for (row <- 0 until length) {
       for (col <- 0 until width) {
@@ -18,39 +21,53 @@ class WorldGrid(length: Int, width: Int) {
     var row = 0
 
     while (row < length) {
-
       val terrainType = random.nextInt(4)
 
-
-      val height: Int = terrainType match{
+      val height: Int = terrainType match {
         case States.ROAD | States.WATER => 2
         case States.TREE | States.GRASS => 1
       }
 
       var safeType = terrainType
-      if (row + height > length){
-        safeType = States.GRASS
-      }
+      if (row + height > length) safeType = States.GRASS
 
-      var safeHeight = height
-      if (row + height > length) {
-        safeHeight = 1
-      }
+      var safeHeight = if (row + height > length) 1 else height
+
+      // On définit une direction aléatoire pour ce bloc
+      var dir = if (random.nextBoolean()) 1 else -1
 
       for (i <- 0 until safeHeight) {
-        for (col <- 0 until width) {
 
+        // ASTUCE : Si on a un bloc d'eau de 2 lignes, on inverse la direction de la 2ème ligne
+        // pour créer un effet de slalom (Gauche / Droite)
+        if (safeType == States.WATER && i > 0) {
+          dir = -dir
+        }
+        rowDirections(row + i) = dir
 
-          if (safeType == States.ROAD && random.nextInt(100) < 15) {
+        // Utilisation de while pour gérer les paires de bûches
+        var col = 0
+        while (col < width) {
+          if (safeType == States.ROAD && random.nextInt(100) < 25) {
             gridToFill(row + i)(col) = States.CAR
-          } else {
-
-            gridToFill(row + i)(col) = safeType
+            col += 1
           }
-
+          else if (safeType == States.WATER && random.nextInt(100) < 25) {
+            // On pose une bûche
+            gridToFill(row + i)(col) = States.LOG
+            // On essaie de poser la suite immédiatement (taille 2)
+            if (col + 1 < width) {
+              gridToFill(row + i)(col + 1) = States.LOG
+              col += 2
+            } else {
+              col += 1
+            }
+          } else {
+            gridToFill(row + i)(col) = safeType
+            col += 1
+          }
         }
       }
-      row += safeHeight
       row += safeHeight
     }
     gridToFill
@@ -65,8 +82,10 @@ class WorldGrid(length: Int, width: Int) {
   }
 
   def scrollDown(): Unit = {
+    // 1. On décale la grille ET les directions
     for (i <- length - 1 until 0 by -1) {
       grid(i) = grid(i - 1)
+      rowDirections(i) = rowDirections(i - 1) // La direction suit la ligne
     }
 
     val newRow = new Array[Int](width)
@@ -74,70 +93,114 @@ class WorldGrid(length: Int, width: Int) {
     var terrainType = States.GRASS
 
     val typeBelow = grid(1)(0)
-
     val typeTwoBelow = if (length > 2) grid(2)(0) else -1
 
     if (typeBelow == States.ROAD || typeBelow == States.WATER) {
-
-      if (typeBelow != typeTwoBelow) {
-
-        terrainType = typeBelow
-      } else {
-
-        terrainType = random.nextInt(4)
-      }
-    }
-    else {
+      if (typeBelow != typeTwoBelow) terrainType = typeBelow
+      else terrainType = random.nextInt(4)
+    } else {
       terrainType = random.nextInt(4)
     }
 
-
-    for (col <- 0 until width) {
-      if (terrainType == States.ROAD && random.nextInt(100) < 15) {
-        newRow(col) = States.CAR
-      } else {
-        newRow(col) = terrainType
-      }
+    // 2. On définit la direction de la nouvelle ligne (index 0)
+    // Si la nouvelle ligne est de l'eau et celle du dessous aussi, on inverse le sens !
+    if (terrainType == States.WATER && typeBelow == States.WATER) {
+      rowDirections(0) = -rowDirections(1)
+    } else {
+      rowDirections(0) = if (random.nextBoolean()) 1 else -1
     }
 
+    // 3. Remplissage avec gestion des bûches doubles
+    var col = 0
+    while (col < width) {
+      if (terrainType == States.ROAD && random.nextInt(100) < 15) {
+        newRow(col) = States.CAR
+        col += 1
+      }
+      else if (terrainType == States.WATER && random.nextInt(100) < 25) {
+        newRow(col) = States.LOG
+        if (col + 1 < width) {
+          newRow(col + 1) = States.LOG
+          col += 2
+        } else {
+          col += 1
+        }
+      }
+      else {
+        newRow(col) = terrainType
+        col += 1
+      }
+    }
     grid(0) = newRow
   }
 
-  // Déplacement des voitures vers la droite
   def moveCars(): Unit = {
     val random = new Random()
-
     for (y <- 0 until length) {
-
       for (x <- width - 1 to 0 by -1) {
-
         if (grid(y)(x) == States.CAR) {
-
-          if (x + 1 >= width) {
-            grid(y)(x) = States.ROAD
-          }
-
+          if (x + 1 >= width) grid(y)(x) = States.ROAD
           else if (grid(y)(x + 1) != States.CAR) {
             grid(y)(x + 1) = States.CAR
             grid(y)(x) = States.ROAD
           }
         }
       }
-
       if (grid(y)(0) == States.ROAD) {
-        if (random.nextInt(100) < 5) {
-          grid(y)(0) = States.CAR
+        if (random.nextInt(100) < 15) grid(y)(0) = States.CAR
+      }
+    }
+  }
+
+  def moveLogs(): Unit = {
+    val random = new Random()
+
+    for (y <- 0 until length) {
+
+      // ON UTILISE LA DIRECTION STOCKÉE (et non y % 2)
+      if (rowDirections(y) == 1) {
+        // --- Vers la DROITE ---
+        for (x <- width - 1 to 0 by -1) {
+          if (grid(y)(x) == States.LOG) {
+            if (x + 1 >= width) grid(y)(x) = States.WATER
+            else if (grid(y)(x + 1) != States.LOG) {
+              grid(y)(x + 1) = States.LOG
+              grid(y)(x) = States.WATER
+            }
+          }
+        }
+        // Apparition Droite (force taille 2)
+        if (grid(y)(0) == States.WATER) {
+          val isHeadAtOne = width > 1 && grid(y)(1) == States.LOG && (width <= 2 || grid(y)(2) != States.LOG)
+          if (isHeadAtOne) grid(y)(0) = States.LOG
+          else if ((width <= 1 || grid(y)(1) != States.LOG) && random.nextInt(100) < 25) grid(y)(0) = States.LOG
+        }
+
+      } else {
+        // --- Vers la GAUCHE ---
+        for (x <- 0 until width) {
+          if (grid(y)(x) == States.LOG) {
+            if (x - 1 < 0) grid(y)(x) = States.WATER
+            else if (grid(y)(x - 1) != States.LOG) {
+              grid(y)(x - 1) = States.LOG
+              grid(y)(x) = States.WATER
+            }
+          }
+        }
+        // Apparition Gauche (force taille 2)
+        if (grid(y)(width - 1) == States.WATER) {
+          val isHeadAtEdge = width > 1 && grid(y)(width - 2) == States.LOG && (width <= 2 || grid(y)(width - 3) != States.LOG)
+          if (isHeadAtEdge) grid(y)(width - 1) = States.LOG
+          else if ((width <= 1 || grid(y)(width - 2) != States.LOG) && random.nextInt(100) < 25) grid(y)(width - 1) = States.LOG
         }
       }
     }
   }
 
-
   def collisionSystemPredict(playerPos: (Int, Int), nextPos: (Int, Int)): Boolean = {
     val nextPoseState = getCell(nextPos._1, nextPos._2)
     nextPoseState == States.CAR
   }
-
 
   def collisionSystemReact(player: Player): Boolean = {
     val (x, y) = player.getPos()
